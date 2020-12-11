97b9e29b121fc79fc1513cd28976eef49689d238
derrek-gass <dardcore@gmail.com>
2020-12-11 13:52:10 -0800

    string formatting

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,6 +1,19 @@
-weight = input('Weight in lbs: ')
-weight_kg =  float(weight) * 0.45
-print("weight in kg: " + str(weight_kg))
+#weight = input('Weight in lbs: ')
+#weight_kg =  float(weight) * 0.45
+#rint("weight in kg: " + str(weight_kg))
 
 course = 'Python for "Beginners"'
-print(course[::-1])
+#print(course[::-1])
+
+first = 'Derrek'
+last = 'Gass'
+msg = f'{first} [{last}] is a coder.'
+#print(msg)
+
+#print(len(course))
+#print(course.find('"'))
+
+print(course.replace('Beginners', 'Absolute Beginners'))
+print(course.replace('P', 'C'))
+
+print ('python' in course)  # checks for existence of string inside other string
